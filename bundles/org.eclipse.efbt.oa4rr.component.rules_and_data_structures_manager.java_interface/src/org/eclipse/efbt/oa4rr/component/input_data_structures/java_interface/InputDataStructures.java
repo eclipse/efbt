02cd32690c4b5b8ad988832c86d6859ad7fc9464
Neil Mackenzie <neil@birdsoftwaresolutions.com>
2020-04-12 14:05:26 +0100

    Minor fixes
    https://github.com/eclipse/efbt/issues/90

--- bundles/org.eclipse.efbt.oa4rr.component.rules_and_data_structures_manager.java_interface/src/org/eclipse/efbt/oa4rr/component/input_data_structures/java_interface/InputDataStructures.java
+++ bundles/org.eclipse.efbt.oa4rr.component.rules_and_data_structures_manager.java_interface/src/org/eclipse/efbt/oa4rr/component/input_data_structures/java_interface/InputDataStructures.java
@@ -3,7 +3,7 @@ package org.eclipse.efbt.oa4rr.component.input_data_structures.java_interface;
 import org.eclipse.efbt.oa4rr.component.common.java_interface.ReadOnlyModuleSet;
 
 import column_structures.ColumnStructureModule;
-import module_management.Module;
+
 
 public interface InputDataStructures extends ReadOnlyModuleSet<ColumnStructureModule> {
 	
