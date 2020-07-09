a3db64be982cac89deecf40a5d663a40a8c24da0
Neil Mackenzie <neil@birdsoftwaresolutions.com>
2020-07-09 16:56:08 +0100

    Convert SQLDeveloper models to Ecore models
    https://github.com/eclipse/efbt/issues/172

---
+++ org.eclipse.efbt.parent/org.eclipse.efbt.ldmtools.sqldevconvertor/src/org/eclipse/efbt/ldmtools/sqldevconvertor/SQLDevConverter.java
@@ -0,0 +1,22 @@
+package org.eclipse.efbt.ldmtools.sqldevconvertor;
+
+public class SQLDevConverter {
+	
+	public static void convert(String fileDirectory) {
+		// get the csv files
+		// for each entity make an Ecore EClass
+		// for each attribute add an attribute to the correct Entity
+		// the attribute should have the correct type, which may be a specific enumeration
+		// for each inheritance relationship, add the inheritence 
+		// for each relationship, add a non-containment relationship with the correct cardinality.
+		///for each domain make an enumeration
+		
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		//read in files from input direc
+	}
+
+}
