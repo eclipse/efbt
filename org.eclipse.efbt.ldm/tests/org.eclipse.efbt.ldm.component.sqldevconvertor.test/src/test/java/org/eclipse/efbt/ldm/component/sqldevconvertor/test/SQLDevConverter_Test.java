4dfda27348b9fe3321e82476927cf44e1ef9d94d
Neil Mackenzie <neil@birdsoftwaresolutions.com>
2020-09-15 14:11:10 +0100

    Split LDM tools into its own directory #242
    https://github.com/eclipse/efbt/issues/242

---
+++ org.eclipse.efbt.ldm/tests/org.eclipse.efbt.ldm.component.sqldevconvertor.test/src/test/java/org/eclipse/efbt/ldm/component/sqldevconvertor/test/SQLDevConverter_Test.java
@@ -0,0 +1,20 @@
+package org.eclipse.efbt.ldm.component.sqldevconvertor.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.eclipse.efbt.ldm.component.sqldevconvertor.SQLDevConverter;
+
+public class SQLDevConverter_Test {
+
+	 @Test
+	    public void exampleTest() {
+		 SQLDevConverter tester = new SQLDevConverter(); // SQLDevConverter is tested
+
+	        // assert statements
+	        assertEquals(0, 0, "zero equals zero");
+	      
+	    }
+	
+}
