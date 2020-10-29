0244a2acfe081136bc4796fa5421e8cc294b2c88
Neil Mackenzie <neil@birdsoftwaresolutions.com>
2020-10-29 16:36:32 +0000

    Add platform call for LDM creator #348
    https://github.com/eclipse/efbt/issues/348

--- org.eclipse.efbt.controller/plugins/org.eclipse.efbt.controller.core.component.core/src/org/eclipse/efbt/controller/core/component/core/Controller.java
+++ org.eclipse.efbt.controller/plugins/org.eclipse.efbt.controller.core.component.core/src/org/eclipse/efbt/controller/core/component/core/Controller.java
@@ -2,9 +2,11 @@ package org.eclipse.efbt.controller.core.component.core;
 
 
 
+import org.eclipse.efbt.controller.core.model.platform_call.ConvertSQLDeveloperModelToEcore;
 import org.eclipse.efbt.controller.core.model.platform_call.PlatformCall;
 import org.eclipse.efbt.controller.core.model.platform_call.Platform_callPackage;
 import org.eclipse.efbt.controller.core.model.platform_call.TranslateBIRDToCocason;
+import org.eclipse.efbt.ldm.component.sqldevconvertor.SQLDevConverter;
 import org.eclipse.efbt.cocason.component.export_smcubes.ImportAndExportSMCubesToNewCocason;
 
 public class Controller {
@@ -16,12 +18,23 @@ public class Controller {
 		{
 			translateBIRDToCocason((TranslateBIRDToCocason) call);
 		}
+		if (call instanceof ConvertSQLDeveloperModelToEcore)
+		{
+			convertSQLDeveloperModelToEcore((ConvertSQLDeveloperModelToEcore) call);
+		}
 	}
 	
 	private static void translateBIRDToCocason(TranslateBIRDToCocason call) {
 		// TODO Auto-generated method stub
 		ImportAndExportSMCubesToNewCocason.importAndExportSMCubes2(call.getInputDirectory(), call.getOutputDirectory(), call.getTestDirectory());
 	}
+	
+	private static void convertSQLDeveloperModelToEcore(ConvertSQLDeveloperModelToEcore call) {
+		// TODO Auto-generated method stub
+		SQLDevConverter.convert(call.getInputDirectory());
+	}
+	
+	
 
 
 
