2df1a21f4ccdb1018485668bb0a3db5c7a790b3d
RBirdwatcher <neil@birdsoftwaresolutions.com>
2020-12-30 09:52:39 +0000

    Revert "refactor eclipse package names #422"
    This reverts commit f6c4bc93d46ad3e2932c0ef40458611b6ce110dd.

---
+++ controller/ldm/tests/org.eclipse.efbt.ldm.component.sqldevconvertor.test/src/test/java/org/eclipse/efbt/ldm/component/sqldevconvertor/test/SQLDevConverter_Test.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Bird Software Solutions Ltd
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Neil Mackenzie - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.efbt.ldm.component.sqldevconvertor.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.eclipse.efbt.ldm.component.sqldevconvertor.SQLDevConverter;
+
+public class SQLDevConverter_Test {
+
+	 @Test
+	    public void exampleTest() {
+		 SQLDevConverter tester = new SQLDevConverter(); // SQLDevConverter is tested
+
+	        // assert statements
+	        assertEquals(0, 0, "zero equals zero");
+	      
+	    }
+	
+}
