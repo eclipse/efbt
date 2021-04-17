01e584b29a62afff1a1ecafcf8e7c19b3ce0d0be
Neil Mackenzie <neil@birdsoftwaresolutions.com>
2021-04-17 16:52:24 +0100

    Replace CoCaSon with plain JSON #569
    https://github.com/eclipse/efbt/issues/569

---
+++ cocalimo/core/model/plugins/org.eclipse.efbt.cocalimo.core.model.module_management/src-gen/org/eclipse/efbt/cocalimo/core/model/module_management/util/Module_managementResourceImpl.java
@@ -0,0 +1,28 @@
+/**
+ */
+package org.eclipse.efbt.cocalimo.core.model.module_management.util;
+
+import org.eclipse.emf.common.util.URI;
+import  org.emfjson.jackson.resource.JsonResource;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * <!-- begin-user-doc -->
+ * The <b>Resource </b> associated with the package.
+ * <!-- end-user-doc -->
+ * @see org.eclipse.efbt.cocalimo.core.model.module_management.util.Module_managementResourceFactoryImpl
+ * @generated NOT
+ */
+public class Module_managementResourceImpl extends JsonResource {
+	/**
+	 * Creates an instance of the resource.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param uri the URI of the new resource.
+	 * @generated NOT
+	 */
+	public Module_managementResourceImpl(URI uri, ObjectMapper mapper ) {
+		super(uri,mapper);
+	}
+
+} //Module_managementResourceImpl
