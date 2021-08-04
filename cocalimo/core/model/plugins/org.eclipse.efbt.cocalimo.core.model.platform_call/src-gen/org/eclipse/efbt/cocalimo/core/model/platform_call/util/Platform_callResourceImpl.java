99142c6d08e2e5c62c2a02ad9c487bbe05aada2b
Neil Mackenzie <neil@birdsoftwaresolutions.com>
2021-08-04 09:40:42 +0100

    ensure that platform_call model uses JSON not XML for serialization #648
    https://github.com/eclipse/efbt/issues/648

--- cocalimo/core/model/plugins/org.eclipse.efbt.cocalimo.core.model.platform_call/src-gen/org/eclipse/efbt/cocalimo/core/model/platform_call/util/Platform_callResourceImpl.java
+++ cocalimo/core/model/plugins/org.eclipse.efbt.cocalimo.core.model.platform_call/src-gen/org/eclipse/efbt/cocalimo/core/model/platform_call/util/Platform_callResourceImpl.java
@@ -3,26 +3,26 @@
 package org.eclipse.efbt.cocalimo.core.model.platform_call.util;
 
 import org.eclipse.emf.common.util.URI;
-
-import org.eclipse.emf.ecore.xmi.impl.XMLResourceImpl;
+import org.emfjson.jackson.resource.JsonResource;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * <!-- begin-user-doc -->
  * The <b>Resource </b> associated with the package.
  * <!-- end-user-doc -->
  * @see org.eclipse.efbt.cocalimo.core.model.platform_call.util.Platform_callResourceFactoryImpl
- * @generated
+ * @generated NOT
  */
-public class Platform_callResourceImpl extends XMLResourceImpl {
+public class Platform_callResourceImpl extends JsonResource {
 	/**
 	 * Creates an instance of the resource.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
 	 * @param uri the URI of the new resource.
-	 * @generated
+	 * @generated NOT
 	 */
-	public Platform_callResourceImpl(URI uri) {
-		super(uri);
+	public Platform_callResourceImpl(URI uri, ObjectMapper mapper ) {
+		super(uri,mapper);
 	}
 
 } //Platform_callResourceImpl
