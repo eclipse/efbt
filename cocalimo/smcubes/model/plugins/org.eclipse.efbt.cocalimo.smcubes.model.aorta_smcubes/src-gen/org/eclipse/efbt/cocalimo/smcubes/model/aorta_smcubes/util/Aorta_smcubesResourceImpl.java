057780bb2887161465fa3ce7970e66c6c1ff1e17
RBirdwatcher <neil@birdsoftwaresolutions.com>
2021-10-24 17:32:15 +0100

    make sure  models are using json for persistance #654
    https://github.com/eclipse/efbt/issues/654

--- cocalimo/smcubes/model/plugins/org.eclipse.efbt.cocalimo.smcubes.model.aorta_smcubes/src-gen/org/eclipse/efbt/cocalimo/smcubes/model/aorta_smcubes/util/Aorta_smcubesResourceImpl.java
+++ cocalimo/smcubes/model/plugins/org.eclipse.efbt.cocalimo.smcubes.model.aorta_smcubes/src-gen/org/eclipse/efbt/cocalimo/smcubes/model/aorta_smcubes/util/Aorta_smcubesResourceImpl.java
@@ -3,26 +3,26 @@
 package org.eclipse.efbt.cocalimo.smcubes.model.aorta_smcubes.util;
 
 import org.eclipse.emf.common.util.URI;
-
-import org.eclipse.emf.ecore.xmi.impl.XMLResourceImpl;
+import  org.emfjson.jackson.resource.JsonResource;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * <!-- begin-user-doc -->
  * The <b>Resource </b> associated with the package.
  * <!-- end-user-doc -->
  * @see org.eclipse.efbt.cocalimo.smcubes.model.aorta_smcubes.util.Aorta_smcubesResourceFactoryImpl
- * @generated
+ * @generated NOT
  */
-public class Aorta_smcubesResourceImpl extends XMLResourceImpl {
+public class Aorta_smcubesResourceImpl extends JsonResource {
 	/**
 	 * Creates an instance of the resource.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
 	 * @param uri the URI of the new resource.
-	 * @generated
+	 * @generated NOT
 	 */
-	public Aorta_smcubesResourceImpl(URI uri) {
-		super(uri);
+	public Aorta_smcubesResourceImpl(URI uri, ObjectMapper mapper ) {
+		super(uri,mapper);
 	}
 
 } //Aorta_smcubesResourceImpl
--- cocalimo/smcubes/model/plugins/org.eclipse.efbt.cocalimo.smcubes.model.input_data/src-gen/org/eclipse/efbt/cocalimo/smcubes/model/input_data/util/Input_dataResourceImpl.java
+++ cocalimo/smcubes/model/plugins/org.eclipse.efbt.cocalimo.smcubes.model.input_data/src-gen/org/eclipse/efbt/cocalimo/smcubes/model/input_data/util/Input_dataResourceImpl.java
@@ -3,26 +3,26 @@
 package org.eclipse.efbt.cocalimo.smcubes.model.input_data.util;
 
 import org.eclipse.emf.common.util.URI;
-
-import org.eclipse.emf.ecore.xmi.impl.XMLResourceImpl;
+import  org.emfjson.jackson.resource.JsonResource;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * <!-- begin-user-doc -->
  * The <b>Resource </b> associated with the package.
  * <!-- end-user-doc -->
  * @see org.eclipse.efbt.cocalimo.smcubes.model.input_data.util.Input_dataResourceFactoryImpl
- * @generated
+ * @generated NOT
  */
-public class Input_dataResourceImpl extends XMLResourceImpl {
+public class Input_dataResourceImpl extends JsonResource {
 	/**
 	 * Creates an instance of the resource.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
 	 * @param uri the URI of the new resource.
-	 * @generated
+	 * @generated NOT
 	 */
-	public Input_dataResourceImpl(URI uri) {
-		super(uri);
+	public Input_dataResourceImpl(URI uri , ObjectMapper mapper ) {
+		super(uri,mapper);
 	}
 
 } //Input_dataResourceImpl
