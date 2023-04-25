74a6000b4d612b7b3ad82d6c894b8b993e6ce449
RBirdwatcher <neil@birdsoftwaresolutions.com>
2023-04-25 11:16:16 +0000

    rename openregspecs as ecore4reg #1083

---
+++ clearMatchingResults.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# clear results script
+
+rm -rf ecore4reg/python/results/ecore/*
+rm -rf ecore4reg/python/results/ecore4reg/*
+rm -rf ecore4reg/python/results/json/*
+rm -rf ecore4reg/python/results/matches/*
+
+
